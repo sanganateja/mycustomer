8f5ddaff504e55cf4030ffccb5f385edbb6d767a
Ravi <sangana575@gmail.com>
2020-04-15 15:29:21 +0530

    Added tables to the coutomer database

---
+++ Tables/Customer.Note.sql
@@ -0,0 +1,10 @@
+CREATE TABLE [Customer].[Note]
+(
+[Note_id] [varchar] (40) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
+[Note] [varchar] (8000) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[InsertionDate] [datetime] NOT NULL CONSTRAINT [DF__Note__InsertionD__4D94879B] DEFAULT (getdate()),
+[ModifiedDate] [datetime] NOT NULL CONSTRAINT [DF__Note__ModifiedDa__4E88ABD4] DEFAULT (getdate())
+) ON [PRIMARY]
+GO
+ALTER TABLE [Customer].[Note] ADD CONSTRAINT [PK__Note__F94C528F5025875A] PRIMARY KEY CLUSTERED  ([Note_id]) ON [PRIMARY]
+GO
