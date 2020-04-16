54a392aab8546f26bf368412d3d40fffbbc38483
sanganateja <34955101+sanganateja@users.noreply.github.com>
2020-04-16 11:27:16 +0530

    Added my id

--- Tables/Customer.Note.sql
+++ Tables/Customer.Note.sql
@@ -3,6 +3,7 @@ CREATE TABLE [Customer].[Note]
 [Note_id] [varchar] (40) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
 [Note] [varchar] (8000) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
 [InsertionDate] [datetime] NOT NULL CONSTRAINT [DF__Note__InsertionD__4D94879B] DEFAULT (getdate()),
+[my_id] [varchar] (40) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
 [ModifiedDate] [datetime] NOT NULL CONSTRAINT [DF__Note__ModifiedDa__4E88ABD4] DEFAULT (getdate())
 ) ON [PRIMARY]
 GO
